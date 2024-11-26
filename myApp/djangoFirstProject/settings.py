f2250799a73ce1986cd18d663268e5e0a684eeca
Nirithle <69992217+Nirithle@users.noreply.github.com>
2024-11-26 19:35:22 +0100

    Add Person & Team

--- myApp/djangoFirstProject/settings.py
+++ myApp/djangoFirstProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nazwaAplikacji.apps.NazwaaplikacjiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
